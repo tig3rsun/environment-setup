3150e42fcf84a4b08e0e321c1f9c52ace2619e57
tig3rsun <zander042+tig3rsun@icloud.com>
2025-01-01 18:20:40 -0700

    Add Wezterm configs & cleanup aerospace configs

---
+++ wezterminal/setup.sh
@@ -0,0 +1,8 @@
+# Install Wezterm and dependencies
+brew install --cask wezterm
+brew install git
+brew install font-meslo-lg-nerd-font
+
+# Create Configs
+touch ~/.wezterm.lua
+cp /.wezterm.lua ~/.wezterm.lua
